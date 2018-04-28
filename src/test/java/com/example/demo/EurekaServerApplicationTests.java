9bfa7cbc54c120cd1d1392b6072d50adff3145be
zzmspring <zzmspring@sina.com>
2018-04-28 23:23:38 +0800

    11

---
+++ src/test/java/com/example/demo/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
